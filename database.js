4e95fb502e97aee329ff609b08ce24d825649618
takhyunKim <kimkih1218@gmail.com>
2021-04-18 19:10:33 +0900

    Add: createRoom logic, room post router

--- database.js
+++ database.js
@@ -3,5 +3,6 @@ const mongoose = require("mongoose");
 mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useFindAndModify: true
+  useFindAndModify: true,
+  useCreateIndex: true
 });
