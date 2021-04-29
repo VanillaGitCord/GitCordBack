357d168e206a7d26516b1805f332e08580ba652a
takhyunKim <kimkih1218@gmail.com>
2021-04-29 17:14:14 +0900

    Correct: port position

--- yServer.js
+++ yServer.js
@@ -9,7 +9,7 @@ const wsReadyStateClosed = 3;
 
 const pingTimeout = 30000;
 
-const port = process.env.PORT || process.env.Y_SERVER_PORT;
+const port = process.env.Y_SERVER_PORT || 4444;
 
 const wss = new ws.Server({ noServer: true });
 
